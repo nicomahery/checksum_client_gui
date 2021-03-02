32522d44998211fab867e67b308ea109d84dbd9b
nicomahery <nicomahery@gmail.com>
2021-03-02 10:45:46 +0100

    new checksum method implemented for faster loading times

--- lib/utils/checksum.dart
+++ lib/utils/checksum.dart
@@ -3,10 +3,14 @@ import 'dart:io';
 import 'package:crclib/catalog.dart';
 import 'package:crclib/crclib.dart';
 
+const num CHECKSUM_END_TO_START_BYTE_OFFSET = 50;
+
 /// Returns the calculated CRC32 checksum for the file passed in [file]
 Future<num> calculateChecksumForFile(File file) async {
-  CrcValue checksumValue = await file.openRead().transform(Crc32Xz()).single;
-  print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()}');
+  int fileLength = file.readAsBytesSync().length;
+  int startOffset = fileLength - CHECKSUM_END_TO_START_BYTE_OFFSET;
+  CrcValue checksumValue = await file.openRead(startOffset).transform(Crc32Xz()).single;
+  print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()} length: $fileLength, startOffset: $startOffset');
   return num.parse(checksumValue.toString());
 }
 
@@ -14,9 +18,11 @@ Future<num> calculateChecksumForFile(File file) async {
 Future<Map<File, num>> calculateChecksumForFileList(List<File> list) async {
   Map<File, num> map = Map();
   for(File file in list) {
-    CrcValue checksumValue = await file.openRead().transform(Crc32Xz()).single;
+    int fileLength = file.readAsBytesSync().length;
+    int startOffset = fileLength - CHECKSUM_END_TO_START_BYTE_OFFSET;
+    CrcValue checksumValue = await file.openRead(startOffset).transform(Crc32Xz()).single;
     map.putIfAbsent(file, () => num.parse(checksumValue.toString()));
-    print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()}');
+    print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()} length: $fileLength, startOffset: $startOffset');
   }
   return map;
 }
@@ -24,7 +30,9 @@ Future<Map<File, num>> calculateChecksumForFileList(List<File> list) async {
 /// Returns the calculated CRC32 checksum for the file located in [filepath]
 Future<num> calculateCRC32XZChecksum(String filepath) async {
   File file = File(filepath);
-  CrcValue checksumValue = await file.openRead().transform(Crc32Xz()).single;
-  //print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()} Api Checksum: $refChecksum');
+  int fileLength = file.readAsBytesSync().length;
+  int startOffset = fileLength - CHECKSUM_END_TO_START_BYTE_OFFSET;
+  CrcValue checksumValue = await file.openRead(startOffset).transform(Crc32Xz()).single;
+  print('Calculate Checksum for file: ${file.path} checksum:${checksumValue.toString()} length: $fileLength, startOffset: $startOffset');
   return num.parse(checksumValue.toString());
 }
